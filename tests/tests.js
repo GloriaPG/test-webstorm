7dd60ed77b5376b71407b31192884a7ad324772d
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2014-11-19 10:19:06 -0600

    Test asynchronous

--- tests/tests.js
+++ tests/tests.js
@@ -14,3 +14,23 @@ describe('Test Framework', function () {
         true.should.eql(true);
     })
 });
+
+describe('Asynchronous testing', function(){
+    var result = false;
+
+    beforeEach(function(done){
+
+        setTimeout(function(){
+            var somethingHere = 2;
+            var somethingElse = 'check';
+
+            result=true;
+            done();
+        },2000);
+    });
+
+    it("Should Not be so hard so this makes it easier.",function(done){
+        result.should.eql(true);
+        done();
+    })
+});
